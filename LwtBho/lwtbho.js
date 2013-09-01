ef842e6635a4019d7614ca9ad4e1473868f34851
chajadan <chajadan@fake.chajadan>
2013-09-01 11:19:15 -0700

    added multi-word term javascript support

--- LwtBho/lwtbho.js
+++ LwtBho/lwtbho.js
@@ -39,6 +39,13 @@ function setSelection(elem, lastterm, curTerm)
 	lastterm.setAttribute('lwtcursel', curTerm);
 	elem.className = elem.className + ' lwtSel';
 }
+function getSelection()
+{
+	var elem = document.getElementById('lwtcursel');
+	if (elem === null)
+		return '';
+	return elem.getAttribute('lwtcursel');
+}
 
 function removeSelection()
 {
@@ -90,7 +97,7 @@ function lwtcontextexit()
 function lwtdivmout(e)
 {
 	fixPageXY(e);
-	var statbox = document.getElementById('lwtinlinestat');
+	var statbox = window.curStatbox;
 	var bVal = XYOnElement(e.pageX, e.pageY, statbox);
 	if (bVal == false)
 	{
@@ -101,7 +108,7 @@ function lwtdivmout(e)
 function lwtmout(e)
 {
 	fixPageXY(e);
-	var statbox = document.getElementById('lwtinlinestat');
+	var statbox = window.curStatbox;
 	if (!XYOnElement(e.pageX, e.pageY, statbox))
 	{
 		lwtcontextexit();
@@ -110,7 +117,7 @@ function lwtmout(e)
 
 function CloseOpenDialogs()
 {
-	document.getElementById('lwtinlinestat').style.display = 'none';
+	window.curStatbox.style.display = 'none';
 }
 
 function lwtkeypress()
@@ -177,19 +184,17 @@ function ExtrapLink(elem, linkForm, curTerm)
 
 function lwtmover(whichid, e, origin)
 {
-/**/		//	alert('entering lwtmover');
 	removeSelection();
 	var divRec = document.getElementById(whichid);
-	var curTerm = divRec.getAttribute('lwtterm');
 	if (divRec == null) {alert('could not locate divRec');}
+	var curTerm = divRec.getAttribute('lwtterm');
 	var lastterm = document.getElementById('lwtlasthovered');
 	if (lastterm == null) {alert('could not loccated lasthovered field');}
 	lastterm.setAttribute('lwtterm', curTerm);
 	lastterm.setAttribute('lwtstat', divRec.getAttribute('lwtstat'));
 	setSelection(origin, lastterm, curTerm);
-	document.attachEvent('onkeydown', lwtkeypress); 
 	fixPageXY(e);
-	lwtshowinlinestat(e, curTerm);
+	lwtshowinlinestat(e, curTerm, origin);
 	var infobox = document.getElementById('lwtinfobox');
 	if (infobox == null) {alert('could not locate infobox to render popup');}
 	infobox.style.left = (e.pageX + 10) + 'px';
@@ -197,33 +202,63 @@ function lwtmover(whichid, e, origin)
 	document.getElementById('lwtinfoboxterm').innerText = curTerm;
 	document.getElementById('lwtinfoboxtrans').innerText = divRec.getAttribute('lwttrans');
 	document.getElementById('lwtinfoboxrom').innerText = divRec.getAttribute('lwtrom');
-/**/		//	infobox.style.display = 'block';
 }
 
-function lwtshowinlinestat(e, curTerm)
+function lwtshowinlinestat(e, curTerm, origin)
 {
-	var statbox = document.getElementById('lwtinlinestat');
+	var statbox = null;
+	var curStat = '';
+
+	if (window.mwTermBegin != null)
+	{
+		statbox = document.getElementById('lwtInlineMWEndPopup');
+		if (window.keydownEventAttached == true)
+		{
+			document.detachEvent('onkeydown', lwtkeypress);
+			window.keydownEventAttached = false;
+		}
+	}
+	else
+	{
+		statbox = document.getElementById('lwtinlinestat');
+		curStat = document.getElementById('lwtcursel').className;
+		ExtrapLink(document.getElementById('lwtextrapdict1'), document.getElementById('lwtdict1').getAttribute('src'), curTerm);
+		ExtrapLink(document.getElementById('lwtextrapdict2'), document.getElementById('lwtdict2').getAttribute('src'), curTerm);
+		ExtrapLink(document.getElementById('lwtextrapgoogletrans'), document.getElementById('lwtgoogletrans').getAttribute('src'), curTerm);
+		if (window.keydownEventAttached != true)
+		{
+			document.attachEvent('onkeydown', lwtkeypress);
+			window.keydownEventAttached = true;
+		}
+	}
+
 	if (statbox == null)
 	{
 		alert('could not locate inline status change popup');
 		return;
 	}
-	statbox.style.left = totalLeftOffset(e.srcElement) + 'px';
-	var inlineTop = (totalTopOffset(e.srcElement) + e.srcElement.offsetHeight);
-	var curStat = document.getElementById('lwtcursel').className;
+
+	window.curStatbox = statbox;
+
+	var posElem = origin;
+	var altElem = e.srcElement;
+	var inlineTop2 = (totalTopOffset(posElem) + posElem.offsetHeight - 2);
+	var inlineTop = totalTopOffset(posElem);
 	if (curStat.indexOf('9') >= 0)
 	{
 		inlineTop -= 2;
 	}
+
+
+	statbox.style.left = totalLeftOffset(posElem) + 'px';
+	document.getElementById('lwtTermTrans').style.width = posElem.offsetWidth + 'px';
+	document.getElementById('lwtTermTrans2').style.width = posElem.offsetWidth + 'px';
 	statbox.style.top = inlineTop + 'px';
+
 	var inlineBottom = inlineTop + statbox.offsetHeight;
-	ExtrapLink(document.getElementById('lwtextrapdict1'), document.getElementById('lwtdict1').getAttribute('src'), curTerm);
-	ExtrapLink(document.getElementById('lwtextrapdict2'), document.getElementById('lwtdict2').getAttribute('src'), curTerm);
-	ExtrapLink(document.getElementById('lwtextrapgoogletrans'), document.getElementById('lwtgoogletrans').getAttribute('src'), curTerm);
-/**/			statbox.style.display = 'block';
+	statbox.style.display = 'block';
 	if (inlineBottom > window.pageYOffset + window.innerHeight)
 		statbox.scrollIntoView(false);
-	}
 }
 
 function traverseDomTree_NextNodeByTagName(elem, aTagName)
@@ -233,7 +268,7 @@ function traverseDomTree_NextNodeByTagName(elem, aTagName)
 		if (elem.firstChild.tagName == aTagName)
 			return elem.firstChild;
 		else
-			return traverseDomTree_NextNodeByTagName(elem.firstChild, tagName);
+			return traverseDomTree_NextNodeByTagName(elem.firstChild, aTagName);
 	}
 
 	var possNextElem = elem.nextSibling;
@@ -246,21 +281,106 @@ function traverseDomTree_NextNodeByTagName(elem, aTagName)
 				return null;
 		}
 
-		return traverseDomTree_NextNodeByTagName(elem.parentNode.nextSibling, tagName);
+		return traverseDomTree_NextNodeByTagName(elem.parentNode.nextSibling, aTagName);
 	}
 
 	if (possNextElem.tagName == aTagName)
 		return possNextElem;
 	else
-		return traverseDomTree_NextNodeByTagName(possNextElem, tagName);
+		return traverseDomTree_NextNodeByTagName(possNextElem, aTagName);
 }
 
-function getMWList(elem)
+function beginMW()
+{
+	window.mwTermBegin = document.getElementById('lwtcursel');
+	lwtcontextexit();
+}
+
+function cancelMW()
+{
+	window.mwTermBegin = null;
+	lwtcontextexit();
+}
+
+function captureMW(newStatus)
+{
+	var newTerm = getChosenMWTerm(document.getElementById('lwtcursel'));
+	cancelMW();
+	if (newTerm != '')
+	{
+		var lastterm = document.getElementById('lwtlasthovered');
+		if (lastterm == null) {alert('could not locate lasthovered field');}
+		lastterm.setAttribute('lwtterm', newTerm);
+		lastterm.setAttribute('lwtstat', '0');
+	}
+	else
+		alert('Not a valid composite term selection.');
+
+	switch(newStatus)
+	{
+		case 49:
+			document.getElementById('lwtsetstat1').click();
+			break;
+		case 50:
+			document.getElementById('lwtsetstat2').click();
+			break;
+		case 51:
+			document.getElementById('lwtsetstat3').click();
+			break;
+		case 52:
+			document.getElementById('lwtsetstat4').click();
+			break;
+		case 53:
+			document.getElementById('lwtsetstat5').click();
+			break;
+		case 87:
+			document.getElementById('lwtsetstat99').click();
+			break;
+		case 73:
+			document.getElementById('lwtsetstat98').click();
+			break;
+	}
+}
+
+function getChosenMWTerm(elemLastPart)
 {
 	var bWithSpaces = false;
 	if (document.getElementById('lwtwithspaces').getAttribute('value') == 'yes')
 		bWithSpaces = true;
 
+	var curTerm = window.mwTermBegin.getAttribute('lwtterm');
+	if (curTerm.length <= 0)
+		return '';
+	var chosenMWTerm = curTerm;
+
+	var elem = window.mwTermBegin;
+	for (var i = 0; i < 8; i++)
+	{
+		elem = traverseDomTree_NextNodeByTagName(elem, 'SPAN');
+
+		if (elem == null)
+			return '';
+
+		curTerm = elem.getAttribute('lwtterm');
+		if (curTerm.length <= 0)
+			i--;
+		else
+		{
+			if (bWithSpaces == true)
+				chosenMWTerm += ' ';
+			
+			chosenMWTerm += curTerm;
+
+			if (elem == elemLastPart)
+				return chosenMWTerm;
+		}
+	}
+
+	return '';
+}
+
+function getPossibleMWTermParts(elem)
+{
 	var mwList = new Array(8);
 	for (var i = 0; i < 8; i++)
 	{
@@ -271,10 +391,8 @@ function getMWList(elem)
 		
 	if (curTerm.length <= 0)
 		return mwList;
-	
-	var accrueTerm = curTerm;
 
-	for (var i = 0; i < 8; i++)
+	for (0; i < 8; i++)
 	{
 		elem = traverseDomTree_NextNodeByTagName(elem, 'SPAN');
 
@@ -288,16 +406,41 @@ function getMWList(elem)
 		if (curTerm.length <= 0)
 			i--;
 		else
-		{
-			if (bWithSpaces == true)
-			{
-				accrueTerm += ' ';
-			}
-
-			accrueTerm += curTerm;
-			mwList[i] = accrueTerm;
-		}
+			mwList[i] = curTerm;
 	}
 
 	return mwList;
+}
+function getMWTermPartsAccrued(elem)
+{
+	var parts = getMWTermParts(elem);
+	var partsAccrued = new Array(8);
+
+	for (var i = 0; i < 8; i++)
+	{
+		mwList[i] = '';
+	}
+
+	var bWithSpaces = false;
+	if (document.getElementById('lwtwithspaces').getAttribute('value') == 'yes')
+		bWithSpaces = true;
+
+	var curTerm = elem.getAttribute('lwtterm');
+		
+	if (curTerm.length <= 0)
+		return mwList;
+
+	var accruedTerm = curTerm;
+
+	for (i = 0; i < 8 && parts[i] != ''; i++)
+	{
+		if (bWithSpaces == true)
+			accruedTerm += ' ';
+
+		accruedTerm += parts[i];
+		partsAccrued[i] = accruedTerm;
+	}
+
+	return partsAccrued;
+	
 }
